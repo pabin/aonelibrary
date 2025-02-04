b8afe37d923e7e7d2175dd35cd72c7b914227844
Pabin R Luitel <pabin2050@gmail.com>
2025-02-04 17:52:32 -0600

    Update add new book feature
    - add book, author and address section to add new book
    - update UI to incorporate all sections
    - handle update functionality for all three models
    - implemented validations to check for existing book

--- src/librarysystem/AddNewBookWindow.java
+++ src/librarysystem/AddNewBookWindow.java
@@ -11,124 +11,193 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 public class AddNewBookWindow extends JFrame implements LibWindow {
     private static final long serialVersionUID = 1L;
     public static final AddNewBookWindow INSTANCE = new AddNewBookWindow();
     private boolean isInitialized = false;
 
-    private JTextField isbnField, titleField, copiesField;
+    private JTextField isbnField, titleField, copiesField, checkoutLengthField;
+    private JTextField afName, aSecName, aPhone, biographyField;
+    private JTextField streetField, cityField, stateField, zipField;
     private JButton saveButton;
-    private JPanel lowerPanel;
-    private JComboBox<String> checkoutLengthComboBox, authorsComboBox;
+    private JPanel bottomPanel;
+    private JComboBox<String> checkoutLengthComboBox;
 
     private AddNewBookWindow() {
     }
 
     public void init() {
         setTitle("Add New Book");
-        setSize(400, 300);
+        setSize(400, 600);
         setLayout(new BorderLayout());
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        defineLowerPanel();
 
-        JPanel panel = new JPanel(new GridLayout(6, 2, 10, 10));
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        JPanel mainPanel = new JPanel(new GridLayout(4, 1, 10, 10));
+//        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        panel.add(new JLabel("ISBN:"));
-        isbnField = new JTextField();
-        panel.add(isbnField);
+        mainPanel.add(createBookPanel());
+        mainPanel.add(createAuthorPanel());
+        mainPanel.add(createAddressPanel());
 
-        panel.add(new JLabel("Title:"));
-        titleField = new JTextField();
-        panel.add(titleField);
-
-        panel.add(new JLabel("Authors:"));
-        String[] authorOptions = {"David", "Julia", "Resham"};
-        authorsComboBox = new JComboBox<>(authorOptions);
-        panel.add(authorsComboBox);
-
-        panel.add(new JLabel("Max Checkout Length (in days):"));
-        String[] checkoutOptions = {"7", "14"};
-        checkoutLengthComboBox = new JComboBox<>(checkoutOptions);
-        panel.add(checkoutLengthComboBox);
-
-        panel.add(new JLabel("Number of Copies:"));
-        copiesField = new JTextField();
-        panel.add(copiesField);
-
-        panel.add(lowerPanel, BorderLayout.SOUTH);
-
-        saveButton = new JButton("Save");
-        panel.add(saveButton);
-
+        saveButton = new JButton("Save Book");
         saveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 handleAddNewBook();
             }
         });
+        saveButton.setPreferredSize(new Dimension(180, 48));
 
-        add(panel, BorderLayout.CENTER);
+        JButton backToMainButn = new JButton("<= Back to Main");
+        backToMainButn.addActionListener(new BackToMainListener());
+        backToMainButn.setPreferredSize(new Dimension(180, 48));
+
+        bottomPanel = new JPanel();
+        bottomPanel.add(backToMainButn);
+        bottomPanel.add(saveButton);
+
+        mainPanel.add(bottomPanel);
+
+        add(mainPanel, BorderLayout.CENTER);
         isInitialized = true;
+    }
+
+    private JPanel createBookPanel() {
+        JPanel bookPanel = new JPanel(new GridLayout(4, 2, 5, 5));
+        bookPanel.setBorder(BorderFactory.createTitledBorder("Book Information"));
+
+        bookPanel.add(new JLabel("ISBN:"));
+        isbnField = new JTextField();
+        bookPanel.add(isbnField);
+
+        bookPanel.add(new JLabel("Title:"));
+        titleField = new JTextField();
+        bookPanel.add(titleField);
+
+        bookPanel.add(new JLabel("Max Checkout Length (in days):"));
+        String[] checkoutOptions = {"7", "14"};
+        checkoutLengthComboBox = new JComboBox<>(checkoutOptions);
+        bookPanel.add(checkoutLengthComboBox);
+
+        bookPanel.add(new JLabel("Number of Copies:"));
+        copiesField = new JTextField();
+        bookPanel.add(copiesField);
+
+        return bookPanel;
+    }
+
+    private JPanel createAuthorPanel() {
+        JPanel authorPanel = new JPanel(new GridLayout(4, 2, 5, 5));
+        authorPanel.setBorder(BorderFactory.createTitledBorder("Author Information"));
+
+        authorPanel.add(new JLabel("First Name:"));
+        afName = new JTextField();
+        authorPanel.add(afName);
+
+        authorPanel.add(new JLabel("Last Name:"));
+        aSecName = new JTextField();
+        authorPanel.add(aSecName);
+
+        authorPanel.add(new JLabel("Phone Number:"));
+        aPhone = new JTextField();
+        authorPanel.add(aPhone);
+
+        authorPanel.add(new JLabel("Biography:"));
+        biographyField = new JTextField();
+        authorPanel.add(biographyField);
+
+        return authorPanel;
+    }
+
+    private JPanel createAddressPanel() {
+        JPanel addressPanel = new JPanel(new GridLayout(4, 2, 5, 5));
+        addressPanel.setBorder(BorderFactory.createTitledBorder("Author's Address Information"));
+
+        addressPanel.add(new JLabel("Street:"));
+        streetField = new JTextField();
+        addressPanel.add(streetField);
+
+        addressPanel.add(new JLabel("City:"));
+        cityField = new JTextField();
+        addressPanel.add(cityField);
+
+        addressPanel.add(new JLabel("State:"));
+        stateField = new JTextField();
+        addressPanel.add(stateField);
+
+        addressPanel.add(new JLabel("Zip:"));
+        zipField = new JTextField();
+        addressPanel.add(zipField);
+
+        return addressPanel;
     }
 
     private void handleAddNewBook() {
         String isbn = isbnField.getText();
         String title = titleField.getText();
-        String authors = (String) authorsComboBox.getSelectedItem();
-        String chekout = (String) checkoutLengthComboBox.getSelectedItem();
+        String checkoutLength = (String) checkoutLengthComboBox.getSelectedItem();
         String copies = copiesField.getText();
 
-        if (!isbn.isEmpty() && !title.isEmpty() && !authors.isEmpty() && !chekout.isEmpty() && !copies.isEmpty()) {
-            // implement author select feature
-            // implement select for checkout len with 7 and 21 option
-            Address add = new Address("120 S. Main", "Fairfield", "IA", "52556");
-            Author author = new Author(authors, "Tesla", "980-203-1234", add, "popular author");
+        String firstName = afName.getText();
+        String lastName = aSecName.getText();
+        String phone = aPhone.getText();
+        String biography = biographyField.getText();
+
+        String street = streetField.getText();
+        String city = cityField.getText();
+        String state = stateField.getText();
+        String zip = zipField.getText();
+
+        DataAccess da = new DataAccessFacade();
+        HashMap<String, Book> books = da.readBooksMap();
+        List<Book> bookList = new ArrayList<>(books.values());
+
+        if (books.get(isbn) != null) {
+            JOptionPane.showMessageDialog(INSTANCE, "Book with given ISBN already exist !!");
+            return;
+        }
+
+        if (!isbn.isEmpty() && !title.isEmpty() && !checkoutLength.isEmpty() && !copies.isEmpty() &&
+                !firstName.isEmpty() && !lastName.isEmpty() && !phone.isEmpty() && !biography.isEmpty() &&
+                !street.isEmpty() && !city.isEmpty() && !state.isEmpty() && !zip.isEmpty()) {
+
+            Address address = new Address(street, city, state, zip);
+            Author author = new Author(firstName, lastName, phone, address, biography);
             List<Author> authorList = new ArrayList<>();
             authorList.add(author);
 
-            Book bookItem = new Book(isbn, title, Integer.parseInt(chekout), authorList);
+            System.out.println("address: " + address);
+            System.out.println("author: " + author);
+
+            Book bookItem = new Book(isbn, title, Integer.parseInt(checkoutLength), authorList);
+            System.out.println("bookItem: " + bookItem);
 
             for (int i = 1; i <= Integer.parseInt(copies); i++) {
                 bookItem.addCopy();
             }
-
-            DataAccess da = new DataAccessFacade();
-            HashMap<String, Book> books = da.readBooksMap();
-            List<Book> bookList = new ArrayList<>(books.values()
-                    .stream()
-                    .toList());
-
-            System.out.println("bookList size before: " + bookList.size());
             bookList.add(bookItem);
             DataAccessFacade.loadBookMap(bookList);
-            System.out.println("bookList size after: " + bookList.size());
 
             JOptionPane.showMessageDialog(INSTANCE, "Book added successfully");
+
+            LibrarySystem.hideAllWindows();
+            LibrarySystem.INSTANCE.setVisible(true);
+            AddNewBookWindow.INSTANCE.dispose();
         } else {
             JOptionPane.showMessageDialog(INSTANCE, "All fields are required!");
         }
-
     }
 
-    public void defineLowerPanel() {
-        JButton backToMainButn = new JButton("<= Back to Main");
-        backToMainButn.addActionListener(new BackToMainListener());
-        lowerPanel = new JPanel();
-        lowerPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        lowerPanel.add(backToMainButn);
-    }
-
-    class BackToMainListener implements ActionListener {
+    static class BackToMainListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent evt) {
             LibrarySystem.hideAllWindows();
             LibrarySystem.INSTANCE.setVisible(true);
-            AddNewBookWindow.INSTANCE.dispose(); // Dispose the current window
+            AddNewBookWindow.INSTANCE.dispose();
         }
     }
 
@@ -142,4 +211,3 @@ public class AddNewBookWindow extends JFrame implements LibWindow {
         isInitialized = val;
     }
 }
-
